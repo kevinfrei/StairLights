dcdf83829d65fc90f6a5c1eaff63d07f890fb5bb
Kevin Frei <kevinfrei@hotmail.com>
2023-11-29 17:19:48 -0800

    Got some basics in place, now

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,23 +1,26 @@
 /*
-  WiFiAccessPoint.ino creates a WiFi access point and provides a web server on it.
+  WiFiAccessPoint.ino creates a WiFi access point and provides a web server on
+  it.
 
   Steps:
   1. Connect to the access point "yourAp"
-  2. Point your web browser to http://192.168.4.1/H to turn the LED on or http://192.168.4.1/L to turn it off
-     OR
-     Run raw TCP "GET /H" and "GET /L" on PuTTY terminal with 192.168.4.1 as IP address and 80 as port
+  2. Point your web browser to http://192.168.4.1/H to turn the LED on or
+  http://192.168.4.1/L to turn it off OR Run raw TCP "GET /H" and "GET /L" on
+  PuTTY terminal with 192.168.4.1 as IP address and 80 as port
 
   Created for arduino-esp32 on 04 July, 2018
   by Elochukwu Ifediora (fedy0)
 */
 
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <WiFiAP.h>
 #include "webfiles.h"
+#include <WiFi.h>
+#include <WiFiAP.h>
+#include <WiFiClient.h>
 
 #ifndef LED_BUILTIN
-#define LED_BUILTIN 2   // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
+#define LED_BUILTIN \
+  2 // Set the GPIO pin where you connected your test LED or comment this line
+    // out if your dev board has a built-in LED
 #endif
 
 // I need a map of paths to 'web files':
@@ -25,11 +28,10 @@
 // Since it's static, I can generate it from the create-react-app "build" step!
 
 // Set these to your desired credentials.
-const char *ssid = "StairwayLights";
-const char *password = NULL; // "deadbeef";
+const char* ssid = "StairwayLights";
+const char* password = NULL; // "deadbeef";
 
 WiFiServer server(80);
-
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -42,7 +44,8 @@ void setup() {
   // a valid password must have more than 7 characters
   if (!WiFi.softAP(ssid, password)) {
     log_e("Soft AP creation failed.");
-    while(1);
+    while (1)
+      ;
   }
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
@@ -52,16 +55,19 @@ void setup() {
   Serial.println("Server started");
 }
 
-bool HandleGet(const String &get, WiFiClient &client) {
+bool HandleGet(const String& get, WiFiClient& client) {
   // Get the file name requested from the get line
   Serial.println(get);
-  size_t pos = get.indexOf(" HTTP/");
+  size_t protocolPos = get.indexOf(" HTTP/");
+  size_t questionPos = get.indexOf('?');
+  size_t pos = std::min(protocolPos, questionPos);
   if (pos < 6) {
     Serial.println("Nope");
     return false;
   }
   Serial.printf("Searching (pos = %d)!", pos);
-  for (const WebMap &item: FileList) {
+  for (const WebMap& item : FileList) {
+    // Starts with 'GET /': 5 characters, right?
     if (item.pathLen == pos - 5 && get.indexOf(item.path) == 5) {
       Serial.printf("Got %s", item.path);
       // Should I break the files up into pieces to send?
@@ -78,25 +84,27 @@ bool HandleGet(const String &get, WiFiClient &client) {
 }
 
 void loop() {
-  WiFiClient client = server.accept();   // listen for incoming clients
+  WiFiClient client = server.accept(); // listen for incoming clients
 
-  if (client) {                             // if you get a client,
-    Serial.println("New Client");           // print a message out the serial port
+  if (client) { // if you get a client,
+    Serial.println("New Client"); // print a message out the serial port
     String GET = "";
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
+    String currentLine =
+      ""; // make a String to hold incoming data from the client
+    while (client.connected()) { // loop while the client's connected
+      if (client.available()) { // if there's bytes to read from the client,
+        char c = client.read(); // read a byte, then
+        Serial.write(c); // print it out the serial monitor
+        if (c == '\n') { // if the byte is a newline character
 
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
+          // if the current line is blank, you got two newline characters in a
+          // row. that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200
+            // OK) and a content-type so the client knows what's coming, then a
+            // blank line:
             if (!HandleGet(GET, client)) {
-              client.println("HTTP/1.1 404 not found");
+              client.println("HTTP/1.1 404 Not Found");
               // The HTTP response ends with another blank line:
               client.println();
             }
@@ -110,17 +118,20 @@ void loop() {
             }
             currentLine = "";
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+        } else if (c != '\r') { // if you got anything else but a carriage
+                                // return character,
+          currentLine += c; // add it to the end of the currentLine
         }
 
         /*
         // Check to see if the client request was "GET /H" or "GET /L":
         if (currentLine.endsWith("GET /H")) {
-          digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED on
+          digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED
+        on
         }
         if (currentLine.endsWith("GET /L")) {
-          digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
+          digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED
+        off
         }
         */
       }
--- scripts/make-web-content.ts
+++ scripts/make-web-content.ts
@@ -1,11 +1,65 @@
+import { promises as fsp } from 'fs';
+
 // What to do here, I wonder...
 
+type MimeFile = { mime: string; name: string };
+type PathBufInfo = MimeFile & { pathinfo: string };
+type FullBufInfo = PathBufInfo & { contentinfo: { len: number; name: string } };
+
+async function getFileList(): Promise<MimeFile[]> {
+  return [];
+}
+
+function CleanPath(p: string, names: Set<string>): string {
+  let res = p.replace(/[^a-zA-Z0-9]/g, '_').toLowerCase();
+  if (names.has(res)) {
+    let number = 2;
+    while (names.has(`${res}${number}`)) {
+      number++;
+    }
+    res = `${res}${number}`;
+  }
+  names.add(res);
+  return res;
+}
+
+function generatePathsList(files: MimeFile[]): PathBufInfo[] {
+  const res: PathBufInfo[] = [];
+  const names = new Set<string>();
+  console.log('namespace Paths {');
+  files.forEach((f: MimeFile) => {
+    const pathinfo = CleanPath(f.name, names);
+    console.log(`  constexpr char ${pathinfo}[] = "${f.name}";`);
+    res.push({ ...f, pathinfo });
+  });
+  console.log('}');
+  return res;
+}
+
+async function generateContents(files: PathBufInfo[]): Promise<FullBufInfo[]> {
+  return [];
+}
 
 async function main() {
   // Read all the static files from the 'build' directory
-  // generate a header named "webfiles.h"
-
-
+  // generate a header named "webcontent.h"
+  // First get the file list
+  const files: MimeFile[] = await getFileList();
+  // From the file list, spit out the file names into the "Paths" namespace:
+  const filePaths: PathBufInfo[] = await generatePathsList(files);
+  // Again, from the file list, put the content of the files into the "Contents" namespace:
+  const contentInfo: FullBufInfo[] = await generateContents(filePaths);
+  // Finally, generate the WebMap linear lookup table
+  // It's a linear lookup because I just don't expect very much content, and a linear search
+  // of the paths is fine for now.
+  console.log(`constexpr WebMap FileList[${contentInfo.length}] {`);
+  contentInfo.forEach((i: FullBufInfo) => {
+    console.log(`  {Mime::${i.mime},`);
+    console.log(`    {${i.pathinfo.length}, Paths::${i.pathinfo}},`);
+    console.log(`    {${i.contentinfo.len}, Contents::${i.contentinfo.name}}`);
+    console.log(`  },`);
+  });
+  console.log('};');
 }
 
-main().catch(console.error)
+main().catch(console.error);
