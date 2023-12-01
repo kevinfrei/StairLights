43a54fd61ec953d4586d7590608b9519cc89ce4e
Kevin Frei <kevinfrei@hotmail.com>
2023-12-01 11:07:33 -0800

    Refactored the sketch a bit. Not sure if I'm going to keep it in Arduino or migrate to a Makefile

--- controller/controller.ino
+++ controller/controller.ino
@@ -12,10 +12,11 @@
   by Elochukwu Ifediora (fedy0)
 */
 
-#include "webfiles.h"
 #include <WiFi.h>
 #include <WiFiAP.h>
 #include <WiFiClient.h>
+
+#include "server.h"
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN \
@@ -30,8 +31,6 @@
 // Set these to your desired credentials.
 const char* ssid = "StairwayLights";
 const char* password = NULL; // "deadbeef";
-
-WiFiServer server(80);
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -55,127 +54,6 @@ void setup() {
   Serial.println("Server started");
 }
 
-bool HandleGet(const String& get, WiFiClient& client) {
-  // Get the file name requested from the get line
-  Serial.println(get);
-  int32_t protocolPos = get.indexOf(" HTTP/");
-  int32_t questionPos = get.indexOf('?');
-  int32_t pos = std::max(protocolPos, questionPos);
-  if (pos < 5) {
-    Serial.printf("Nope: %s (%d, %d)\n", get.c_str(), protocolPos, questionPos);
-    return false;
-  }
-  Serial.printf("Searching (pos = %d)!\n", pos);
-  for (const WebFile& item : FileList) {
-    // Starts with 'GET /': 5 characters, right?
-
-    if (item.pathname.len == pos - 5 && 
-        (get.indexOf(item.pathname.data) == 5 || 
-          (item.pathname.len == 0 && protocolPos == 5))) {
-      Serial.printf("Got %s", item.pathname.data);
-      // Should I break the files up into pieces to send?
-      client.println("HTTP/1.1 200 OK");
-      client.printf("Content-type:%s\n", item.mimeType);
-      client.println();
-      client.write(item.content.data, item.content.len);
-      client.println();
-      return true;
-    }
-  }
-  // Serial.println("Nothing found");
-  return false;
-}
-
-bool HandlePost(const String& post, WiFiClient& client) {
-  // Get the file name requested from the get line
-  Serial.println(post);
-  int32_t protocolPos = post.indexOf(" HTTP/");
-  int32_t questionPos = post.indexOf('?');
-  int32_t pos = std::max(protocolPos, questionPos);
-  if (pos < 7) {
-    Serial.printf(
-      "Nope: %s (%d, %d)\n", post.c_str(), protocolPos, questionPos);
-    return false;
-  }
-  if (post.indexOf("api_") == 6) {
-    String s = post.substring(6 + 4, pos);
-    Serial.printf("API: '%s' (%d)\n", s.c_str(), pos);
-    client.println("HTTP/1.1 200 OK");
-    client.printf("Content-type:text/plain\n");
-    client.println();
-    client.write("OK", 3);
-    client.println();
-    return true;
-  }
-  // Serial.println("Nothing found");
-  return false;
-}
-
 void loop() {
-  WiFiClient client = server.accept(); // listen for incoming clients
-
-  if (client) { // if you get a client,
-    // Serial.println("New Client"); // print a message out the serial port
-    String GET = "";
-    String POST = "";
-    String currentLine =
-      ""; // make a String to hold incoming data from the client
-    while (client.connected()) { // loop while the client's connected
-      if (client.available()) { // if there's bytes to read from the client,
-        char c = client.read(); // read a byte, then
-        // Serial.write(c); // print it out the serial monitor
-        if (c == '\n') { // if the byte is a newline character
-
-          // if the current line is blank, you got two newline characters in a
-          // row. that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200
-            // OK) and a content-type so the client knows what's coming, then a
-            // blank line:
-            if (GET.length() != 0) {
-              if (!HandleGet(GET, client)) {
-                client.println("HTTP/1.1 404 Not Found");
-                // The HTTP response ends with another blank line:
-                client.println();
-              }
-              GET = "";
-            } else if (POST.length() != 0) {
-              if (!HandlePost(POST, client)) {
-                client.println("HTTP/1.1 404 Not Found");
-                // The HTTP response ends with another blank line:
-                client.println();
-              }
-              POST = "";
-            }
-            break;
-          } else {
-            if (currentLine.startsWith("GET /")) {
-              GET = currentLine;
-            } else if (currentLine.startsWith("POST /")) {
-              POST = currentLine;
-            }
-            currentLine = "";
-          }
-        } else if (c != '\r') { // if you got anything else but a carriage
-                                // return character,
-          currentLine += c; // add it to the end of the currentLine
-        }
-
-        /*
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED
-        on
-        }
-        if (currentLine.endsWith("GET /L")) {
-          digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED
-        off
-        }
-        */
-      }
-    }
-    // close the connection:
-    client.stop();
-    // Serial.println("Client Disconnected.");
-  }
+  ServerLoop();
 }
---
+++ controller/get-handler.h
@@ -0,0 +1,33 @@
+#include "webfiles.h"
+
+// This guy serves up static content, encoded by the node script
+bool HandleGet(const String& get, WiFiClient& client) {
+  // Get the file name requested from the get line
+  Serial.println(get);
+  int32_t protocolPos = get.indexOf(" HTTP/");
+  int32_t questionPos = get.indexOf('?');
+  int32_t pos = std::max(protocolPos, questionPos);
+  if (pos < 5) {
+    Serial.printf("Nope: %s (%d, %d)\n", get.c_str(), protocolPos, questionPos);
+    return false;
+  }
+  Serial.printf("Searching (pos = %d)!\n", pos);
+  for (const WebFile& item : FileList) {
+    // Starts with 'GET /': 5 characters, right?
+
+    if (item.pathname.len == pos - 5 &&
+        (get.indexOf(item.pathname.data) == 5 ||
+          (item.pathname.len == 0 && protocolPos == 5))) {
+      Serial.printf("Got %s", item.pathname.data);
+      // Should I break the files up into pieces to send?
+      client.println("HTTP/1.1 200 OK");
+      client.printf("Content-type:%s\n", item.mimeType);
+      client.println();
+      client.write(item.content.data, item.content.len);
+      client.println();
+      return true;
+    }
+  }
+  // Serial.println("Nothing found");
+  return false;
+}
